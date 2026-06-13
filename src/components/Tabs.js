02ac4c74573f3b12b93d9b1d27add497c6d1815e
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-06-13 23:24:13 +0530

    tabs2

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Tabs = () => {
   return (
     <div className="tabs">
-      <button className="tab" id="Home-tab">Home</button>
-      <button className="tab" id="About-tab">About</button>
-      <button className="tab" id="Features-tab">Features</button>
+      <button className="tab" id="Home-Tab">Home</button>
+      <button className="tab" id="About-Tab">About</button>
+      <button className="tab" id="Features-Tab">Features</button>
       <h1 className="viewport">Pages Go Here</h1>
     </div>
   )
