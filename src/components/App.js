d381dc27a61be374dff40d9c18925a40b0fd3f77
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-06-13 23:20:53 +0530

    tabs-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import "./../styles/App.css"
+import Tabs from "./Tabs";
 
+const App = () => {
+  return (
+    <div>
+        <Tabs/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Tabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tabs = () => {
+  return (
+    <div className="tabs">
+      <button className="tab" id="Home-tab">Home</button>
+      <button className="tab" id="About-tab">About</button>
+      <button className="tab" id="Features-tab">Features</button>
+      <h1 className="viewport">Pages Go Here</h1>
+    </div>
+  )
+}
+
+export default Tabs
